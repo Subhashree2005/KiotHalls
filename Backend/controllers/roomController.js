f4f894abb8ad6d94ec2c098ccc50f53a48903418
subhashree <subhashree47464@gmail.com>
2025-02-06 12:21:56 +0530

    changes

--- Backend/controllers/roomController.js
+++ Backend/controllers/roomController.js
@@ -1,9 +1,23 @@
 const roomModel = require('../models/room');
 
 exports.getRooms = async (req,res,next) => {
-   const rooms = await roomModel.find({});
+
+  const rooms = await roomModel.find({});
        res.json({
         sucess: true,
-        rooms
+        message:'Get rooms Working!'//room
+    
     })
 }
+// get single room API-/api/rooms/room:id
+exports.getSingleRoom = (req,res,next) => {
+console.log(req.params.id,'ID'),
+
+        res.json({
+         sucess: true,
+         message:'Get Single Product working!'
+     })
+ }
+
+
+
--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Homescreen() {
-  return (
-    <div>Homescreen</div>
-  )
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/rooms");
+        console.log(response.data);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div>Homescreen</div>;
 }
 
-export default Homescreen
+export default Homescreen;
